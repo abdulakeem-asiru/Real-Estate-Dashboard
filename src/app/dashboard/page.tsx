2d3a7115c6284d4aff7def237dbc9f28ccf37c0d
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-05-19 00:59:59 +0100

    new commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
         <h2 className='text-[24px] font-medium text-[var(--text-primary)] mb-1'>Dashboard</h2>
         <p className='text-[16px] font-[500] text-[var(--text-secondary)]'>Welcome, Let’s dive into your personalized setup guide.</p>
       </div>
-      <Button className='bg-[var(--primary-color)] p-5'><span><Plus /></span>Add Property</Button>
+      <Button className='bg-[var(--primary-color)] p-5'><span><Plus /></span><span className='hidden md:block'>Add Property</span></Button>
      </div>
     </div>
   )
