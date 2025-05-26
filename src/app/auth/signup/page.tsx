7830161e256d04bdb3db8f0e93cb8424c2776ebe
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-05-26 17:15:31 +0100

    auth page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import google from "@/assets/icon/google.png"
+import apple from "@/assets/icon/apple.png"
+
 import {
   Card,
   CardContent,
@@ -16,26 +19,28 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import Image from 'next/image'
 
-
-const TabsComponent =() =>  {
-  return (
-    <Tabs defaultValue="customer" className="max-w-[500px] w-full">
-      <TabsList className="grid w-full grid-cols-3 h-[60px]">
-        <TabsTrigger value="customer" className='transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Customer</TabsTrigger>
-        <TabsTrigger value="agent" className='transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Agent</TabsTrigger>
-        <TabsTrigger value="company" className=' transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Company</TabsTrigger>
-      </TabsList>
-      <TabsContent value="customer">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
+const CustomerForm = () =>{
+  return(
+     <Card className='border-none shadow-none'>
+          {/* <CardHeader>
+            <CardTitle></CardTitle>
             <CardDescription>
-              Make changes to your account here. Click save when you &apos;re done.
             </CardDescription>
-          </CardHeader>
+          </CardHeader> */}
           <CardContent className="space-y-2">
-            <div className="space-y-1">
+            <div className='grid grid-cols-2 gap-4 justify-center items-start'>
+              <Button className='flex gap-2 h-[50px] bg-transparent border-[var(--border-color)] border-2 text-sm font-medium p-4 hover:bg-transparent hover:scale-[1.1] cursor-pointer text-[var(--text-secondary)] '><Image src={google} alt='google icon' width={30} height={30}/>Sign Up with Google</Button>
+              <Button className='flex gap-2 h-[50px] bg-transparent border-[var(--border-color)] border-2 text-sm font-medium p-4 hover:bg-transparent hover:scale-[1.1] cursor-pointer text-[var(--text-secondary)] '><Image src={apple} alt='google icon' width={30} height={30}/>Sign Up with Apple</Button>
+            </div>
+            <div className="flex items-center gap-4 my-4">
+  <hr className="flex-grow border-[var(--border-color)]" />
+  <span className="text-[var(--text-secondary)] text-[16px]">or</span>
+  <hr className="flex-grow border-[var(--border-color)] " />
+</div>
+
+            <div className="space-y-2">
               <Label htmlFor="name">Name</Label>
               <Input id="name" defaultValue="Pedro Duarte" />
             </div>
@@ -48,6 +53,19 @@ const TabsComponent =() =>  {
             <Button>Save changes</Button>
           </CardFooter>
         </Card>
+  )
+};
+
+const TabsComponent =() =>  {
+  return (
+    <Tabs defaultValue="customer" className="max-w-[500px] w-full">
+      <TabsList className="grid w-full grid-cols-3 h-[50px]">
+        <TabsTrigger value="customer" className='transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Customer</TabsTrigger>
+        <TabsTrigger value="agent" className='transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Agent</TabsTrigger>
+        <TabsTrigger value="company" className=' transition-all data-[state=active]:bg-[var(--primary-color)] data-[state=active]:text-[#ffffff] data-[state=active]:shadow-md'>Company</TabsTrigger>
+      </TabsList>
+      <TabsContent value="customer">
+       <CustomerForm />
       </TabsContent>
       <TabsContent value="agent">
         <Card>
